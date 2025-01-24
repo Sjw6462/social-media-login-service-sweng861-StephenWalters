e928393334d69fc3257b72e5512ecfbc4f95541a
Sjw6462 <sjw6462@psu.edu>
2025-01-24 18:35:53 -0500

    Update server.js

--- server.js
+++ server.js
@@ -39,3 +39,46 @@ app.get('/linkedin/callback', async (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+// Facebook OAuth callback
+app.get('/facebook/callback', async (req, res) => {
+    const authorizationCode = req.query.code;
+
+    if (!authorizationCode) {
+        return res.status(400).send('Authorization code is missing');
+    }
+
+    try {
+        const response = await axios.get('https://graph.facebook.com/v12.0/oauth/access_token', {
+            params: {
+                client_id: process.env.FACEBOOK_CLIENT_ID,
+                redirect_uri: process.env.FACEBOOK_REDIRECT_URI,
+                client_secret: process.env.FACEBOOK_CLIENT_SECRET,
+                code: authorizationCode,
+            },
+        });
+
+        const accessToken = response.data.access_token;
+
+        // Now use this access token to fetch user info
+        const userResponse = await axios.get('https://graph.facebook.com/me', {
+            params: {
+                access_token: accessToken,
+                fields: 'id,name,email,picture',
+            },
+        });
+
+        // Send user data back to frontend
+        res.json({
+            platform: 'Facebook',
+            profile: userResponse.data,
+        });
+    } catch (error) {
+        console.error('Error exchanging authorization code for Facebook:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
